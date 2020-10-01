330f1dbe28de357341ac004fd07db59903ee3ca9
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-01 09:40:26 -0400

    added starting files to contact page

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./contact.css";
+
+function Contact() {
+  return (
+    <div id="contact">
+      <h1>About ZAP</h1>
+      <hr />
+      <p>Zap is an application that allows... TBD</p>
+    </div>
+  );
+}
+
+export default Contact;
