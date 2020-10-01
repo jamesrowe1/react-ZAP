6ae3a558b8868b4f27bac94a7257deb6cab5c43e
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-01 10:47:21 -0400

    added div to form to apply consistent style

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -6,27 +6,29 @@ function Contact() {
   //have to add useHistory and prevent default to redirect user to a home page---that goes before the return statement
 
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Full Name</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+    <div id="contact">
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Tell us your thoughts!</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Tell us your thoughts!</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 }
 
