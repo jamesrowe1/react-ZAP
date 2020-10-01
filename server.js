29e2265b988b410faade9833e7532985e1d7b139
World of Joy <tanmay.sarker@baruchmail.cuny.edu>
2020-10-01 11:14:38 -0400

    server.js added

---
+++ server.js
@@ -0,0 +1,32 @@
+// dendencies
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const passport = require("./authentication/passport");
+
+// setup the port and the express app
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// middlewares for accepting post requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// routes
+app.use(routes);
+
+// setup the mongodb database
+mongoose.connect(process.env.MONGODB_URI || "", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
