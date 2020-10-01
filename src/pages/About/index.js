07918dad5b5a04f76456416541b478ef88cdb2f3
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-01 09:38:57 -0400

    added starter files and exports of about page

---
+++ src/pages/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./about.css";
+
+function About() {
+  return (
+    <div id="About">
+      <h1>About ZAP</h1>
+      <hr />
+      <p>Zap is an application that allows... TBD</p>
+    </div>
+  );
+}
+
+export default About;
