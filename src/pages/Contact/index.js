3368e35b57f2708eec985e9a7420cbb99bd02cc0
Melissa Mercado <melissa.a.merc@gmail.com>
2020-10-01 10:45:34 -0400

    added contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,13 +1,32 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
 import "./contact.css";
 
 function Contact() {
+  //have to add useHistory and prevent default to redirect user to a home page---that goes before the return statement
+
   return (
-    <div id="contact">
-      <h1>About ZAP</h1>
-      <hr />
-      <p>Zap is an application that allows... TBD</p>
-    </div>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Tell us your thoughts!</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
